7243ff2acfaa235137333be3f04f5f822e07ab34
Adarsh <23BAI70616@cuchd.in>
2026-03-15 07:05:46 +0530

    Initial commit - JWT Authentication Spring Boot

---
+++ src/main/java/com/example/jwt_auth/controller/AuthController.java
@@ -0,0 +1,38 @@
+package com.example.jwt_auth.controller;
+
+import com.example.jwt_auth.model.User;
+import com.example.jwt_auth.security.JwtUtil;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final JwtUtil jwtUtil;
+
+    public AuthController(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @PostMapping("/login")
+    public Map<String,String> login(@RequestBody User user){
+
+        if(user.getUsername().equals("admin") &&
+                user.getPassword().equals("1234")){
+
+            String token = jwtUtil.generateToken(user.getUsername());
+
+            return Map.of("token",token);
+        }
+
+        throw new RuntimeException("Invalid Credentials");
+    }
+
+    @GetMapping("/protected")
+    public String protectedRoute(){
+
+        return "Accessed Protected API";
+    }
+}
---
+++ src/main/java/com/example/jwt_auth/security/JwtUtil.java
@@ -0,0 +1,22 @@
+package com.example.jwt_auth.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private String secret = "mysecretkeymysecretkeymysecretkeymysecretkey123456";
+
+    public String generateToken(String username){
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis()+3600000))
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+    }
+}
